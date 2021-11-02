0d390e2c7915a1b45393befb1e6a120c73787979
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-11-02 15:39:02 -0300

    first commit

---
+++ src/view/Principal.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.SomaController;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        int entrada;
+        SomaController dc = new SomaController();
+
+        do {
+            entrada = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre um número entre 1 e 12", JOptionPane.QUESTION_MESSAGE));
+        } while (entrada <= 0 || entrada > 12);
+
+
+        int soma = dc.soma(entrada);
+        System.out.println(soma);
+
+    }
+}
